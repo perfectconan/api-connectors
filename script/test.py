342af4eaf3fc88a142cc83c322821d3863279a47
perfectconan <szperfectconan@gmail.com>
2018-03-22 08:11:40 +0100

    update test.py

--- script/test.py
+++ script/test.py
@@ -1,26 +1,27 @@
 import logging
-import numpy as np
-import pandas as pd
+from time import sleep
 from bitmex_websocket import BitMEXWebsocket
-
-
-class HighResolutionFormatter(logging.Formatter):
-    def formatTime(self, record, datefmt=None):
-        ct = pd.to_datetime(np.int64(record.created * 1000000000))
-        t = ct.strftime("%Y-%m-%d %H:%M:%S.%f")
-        s = "%s%s" % (t, ct.nanosecond)
-        return s
+from websocket import WebSocketException
 
 
 FORMAT = '%(asctime)-15s %(message)s'
-# FORMATTER = HighResolutionFormatter(FORMAT)
 FORMATTER = logging.Formatter(FORMAT)
-FILE_HANDLER = logging.FileHandler('test2.log')
+FILE_HANDLER = logging.FileHandler('test.log')
 FILE_HANDLER.setFormatter(FORMATTER)
 handlers = [FILE_HANDLER]
 logging.basicConfig(format=FORMAT, level=logging.DEBUG, handlers=handlers)
 
 ws = BitMEXWebsocket(endpoint="https://www.bitmex.com/api/v1", symbol="XBTUSD", api_key=None, api_secret=None)
+reconnect_delay = 1
+
 while True:
-    continue
-# ws.exit()
+    try:
+        sleep(1)
+    except WebSocketException:
+        ws.exit()
+        if reconnect_delay < 1024:
+            logging.info("Reconnecting in %s seconds" % reconnect_delay)
+            sleep(reconnect_delay)
+            ws = BitMEXWebsocket(endpoint="https://www.bitmex.com/api/v1", symbol="XBTUSD", api_key=None, api_secret=None)
+            reconnect_delay *= 2
+        raise Exception("Reconnect too many times")
